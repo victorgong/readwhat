808d08a9c98c9e0ee36f5d911ec5e0d6a2f98e90
victoria <gyyshuai@gmail.com>
2016-03-03 15:14:27 +0800

    remove comments

--- api/controllers/books.server.controller.js
+++ api/controllers/books.server.controller.js
@@ -20,26 +20,6 @@ exports.thisWeek = function (request, reply) {
       .exec(function(err,data){
          reply(data.reverse());
        });
-      
-      /*
-  var finalRanks =[];
-  Rank.find({year:year, week:week}).sort('rankVal desc').limit(10).populate('book')
-      .then(function(ranks){
-          Promise.map(ranks,function(rank){
-              return Book.findOne({id:rank.book.id}).populate('comments',{limit:10,sort:'createdAt desc'})
-                         .then(function(book){
-                           var newRank = _.clone(rank);
-                           newRank.book = book;
-                           finalRanks.push(newRank);
-                         });
-
-          }).then(function(){
-              //reply(finalRanks.reverse());
-              reply(_.sortBy(finalRanks,'rankVal'));
-
-          });  
-        });
-        */
   
  };
  exports.getComments = function (request, reply) {
