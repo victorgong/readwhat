71c23503afd7deb01e4075a5989f22217cf56010
victoria <gyyshuai@gmail.com>
2016-03-16 14:56:16 +0800

    update

--- app/js/filters.js
+++ app/js/filters.js
@@ -31,8 +31,7 @@ app.filter('ranknum',[ function() {
 }]);
 app.filter('topweeks',[ function() {
   return function(input){
-    if (undefined === input)
-    {
+    if (undefined === input){
       input = 1;
     }
     input = input ===0?1:input;
