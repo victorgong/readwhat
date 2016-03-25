a08d7578dbd832680799fb742491746cdb04fd23
victoria <gyyshuai@gmail.com>
2016-03-25 09:12:05 +0800

    add tongji

---
+++ app/dist/tj.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?50a1d1c86bf7225622a51fd5b0bea6d4";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
